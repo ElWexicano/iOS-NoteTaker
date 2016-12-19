3b21a227e16b2eed846538b5556857db4a8b9eaf
Shane Doyle <shanebryandoyle@gmail.com>
2016-12-19 14:10:34 +0000

    My first commit

---
+++ NoteTaker/NoteTakerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NoteTakerViewController.swift
+//  NoteTaker
+//
+//  Created by Shane Doyle on 19/12/2016.
+//  Copyright © 2016 Shane Doyle. All rights reserved.
+//
+
+import UIKit
+
+class NoteTakerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "My First Note"
+        return cell
+    }
+    
+}
